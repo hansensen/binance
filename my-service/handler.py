557ba07933b0e39176b8e1c5d4539d56ac38f5ab
Han Jie <jhan1@flexport.com>
2023-10-02 20:46:43 +0800

    add position

--- my-service/handler.py
+++ my-service/handler.py
@@ -17,8 +17,11 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('TradingState')
 
 # Gain and Loss thresholds (as percentages)
-GAIN_THRESHOLD = 0.02  # e.g., 2%
-LOSS_THRESHOLD = 0.01  # e.g., 1%
+GAIN_THRESHOLD = 0.02  # e.g., 2% profit
+LOSS_THRESHOLD = 0.01  # e.g., 1% loss
+
+SHORT_WINDOW = 5  # e.g., 5-minute MA
+LONG_WINDOW = 20  # e.g., 20-minute MA
 
 def save_state_to_dynamodb(symbol, trade_data):
     """Saves trade data to DynamoDB."""
@@ -40,47 +43,73 @@ def get_last_trade_from_dynamodb(symbol):
 
 def execute_trade(symbol="BTCUSDT"):
     try:
-        klines = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC")
+        klines = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1MINUTE, "30 minutes ago UTC")
         if not klines:
             return
 
         close_values = [float(k[4]) for k in klines]
-        average_close = sum(close_values) / len(close_values)
+
+        short_ma = sum(close_values[-SHORT_WINDOW:]) / SHORT_WINDOW
+        long_ma = sum(close_values[-LONG_WINDOW:]) / LONG_WINDOW
 
         current_price = float(client.get_symbol_ticker(symbol=symbol)["price"])
-
         last_trade = get_last_trade_from_dynamodb(symbol)
 
-        trade_action = "HOLD"
-        accumulated_gain = Decimal('0')
+        position = None
         last_trade_price = 0
-
         if last_trade:
+            position = last_trade.get('position')
             last_trade_price = float(last_trade.get('price', 0))
             accumulated_gain = Decimal(last_trade.get('accumulated_gain', '0'))
-            percentage_change = (current_price - last_trade_price) / last_trade_price
-
-            if percentage_change >= GAIN_THRESHOLD and current_price > average_close:
-                trade_action = "SELL"
-                accumulated_gain += Decimal(str(percentage_change))
-            elif percentage_change <= -LOSS_THRESHOLD and current_price < average_close:
-                trade_action = "BUY"
-                accumulated_gain += Decimal(str(percentage_change))
         else:
-            trade_action = "BUY_STARTING_AMOUNT"
+            accumulated_gain = Decimal('0')
+
+        trade_action = "HOLD"
+        percentage_change = (current_price - last_trade_price) / last_trade_price if last_trade_price != 0 else 0
+
+        # Gain/Loss control checks
+        if position == "LONG":
+            if percentage_change >= GAIN_THRESHOLD:
+                trade_action = "SELL"
+                position = "SHORT"
+                accumulated_gain += Decimal(str(percentage_change))
+            elif percentage_change <= -LOSS_THRESHOLD:
+                trade_action = "SELL"
+                position = "SHORT"
+                accumulated_gain += Decimal(str(percentage_change))
+        elif position == "SHORT":
+            if percentage_change <= -GAIN_THRESHOLD:
+                trade_action = "BUY"
+                position = "LONG"
+                accumulated_gain -= Decimal(str(percentage_change))
+            elif percentage_change >= LOSS_THRESHOLD:
+                trade_action = "BUY"
+                position = "LONG"
+                accumulated_gain -= Decimal(str(percentage_change))
+
+        if trade_action == "SELL":
+            accumulated_gain = Decimal('0')
+
+        # Dual MA crossover checks (only if no action determined by gain/loss control)
+        if trade_action == "HOLD":
+            if short_ma > long_ma and position != "LONG":
+                trade_action = "BUY"
+                position = "LONG"
+            elif short_ma < long_ma and position != "SHORT":
+                trade_action = "SELL"
+                position = "SHORT"
 
         trade_data = {
             "price": Decimal(str(current_price)),
             "action": trade_action,
+            "position": position,
             "accumulated_gain": accumulated_gain
         }
 
-        # Log the details
         print(f"Current Price: {current_price}")
-        print(f"Last Trade Price: {last_trade_price}")
-        print(f"Average Close (24h): {average_close}")
+        print(f"Short MA: {short_ma}")
+        print(f"Long MA: {long_ma}")
         print(f"Action: {trade_action}")
-        print(f"Accumulated Gain: {accumulated_gain}")
 
         save_state_to_dynamodb(symbol, trade_data)
         return trade_data
